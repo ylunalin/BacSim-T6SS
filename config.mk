52fb2328bb06bd89a56704305e36fdc1aec35344
Y. Luna Lin <y.lin2783@gmail.com>
2021-10-21 17:19:19 +0200

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
-# Replace the compiler commands and the flags with ones on your computing system
-# Compiler and compilation flags
+# The compilers used here are GNU GCC and G++ 9 installed with MacPorts.
+# Replace the compiler commands and the flags with ones on your computing system.
 cc=gcc-mp-9
 cxx=g++-mp-9 -fopenmp
-cflags=-Wall -march=native -ansi -pedantic -O3 -fopenmp -std=c++11 -fPIC -lstdc++fs -Wno-variadic-macros -g
+cflags=-Wall -march=native -ansi -pedantic -O0 -fopenmp -std=c++11 -fPIC -lstdc++fs -Wno-variadic-macros -g
