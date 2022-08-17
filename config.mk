b241984ac97639fe0cac5159833c186d88656e04
Luna Lin <y.lin2783@gmail.com>
2022-08-17 10:13:18 -0400

    Added some compiler flags for a machine running Ubuntu focal, with GNU GCC compilers

--- config.mk
+++ config.mk
@@ -1,5 +1,11 @@
 # The compilers used here are GNU GCC and G++ 9 installed with MacPorts.
 # Replace the compiler commands and the flags with ones on your computing system.
-cc=gcc-mp-9
-cxx=g++-mp-9 -fopenmp
-cflags=-Wall -march=native -ansi -pedantic -O0 -fopenmp -std=c++11 -fPIC -lstdc++fs -Wno-variadic-macros -g
+#cc=gcc-mp-9
+#cxx=g++-mp-9 -fopenmp
+#cflags=-Wall -march=native -ansi -pedantic -O0 -fopenmp -std=c++11 -fPIC -lstdc++fs -Wno-variadic-macros -g
+
+
+# Compiler and compilation flags used on a Linux machine
+cc=gcc
+cxx=g++
+cflags=-Wall -ansi -pedantic -O3 -fopenmp -std=c++11 -fPIC -lstdc++fs -Wno-variadic-macros -g -ggdb -Wuninitialized
